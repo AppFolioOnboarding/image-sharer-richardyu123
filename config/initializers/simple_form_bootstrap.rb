7f5d055e4bb2b11f750f140f6a2cbd50a792c644
Richard Yu <richard.yu@appfolio.com>
2018-08-27 13:58:37 -0700

    ry - fixed rubocop errors

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -13,6 +13,7 @@
 # Dir[Rails.root.join('lib/components/**/*.rb')].each { |f| require f }
 
 # Use this setup block to configure all options available in SimpleForm.
+# rubocop:disable Metrics/BlockLength, LineLength
 SimpleForm.setup do |config|
   # Default class for buttons
   config.button_class = 'btn'
@@ -430,3 +431,4 @@ SimpleForm.setup do |config|
   #   time:          :custom_multi_select
   # }
 end
+# rubocop:enable Metrics/BlockLength, LineLength

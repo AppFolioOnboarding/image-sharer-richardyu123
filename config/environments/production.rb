916c763c531e71d37d83cf909fde4db4a356d106
Richard Yu <richard.yu@appfolio.com>
2018-09-04 11:22:47 -0700

    ry - set up send grid info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,17 +59,17 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "base_#{Rails.env}"
   config.action_mailer.perform_caching = false
-=begin
-  config.action_mailer.smtp_settings = (
-  :user_name => 'ropes@appfolio.com',
-    :password => 'Appfolio5%',
-    :domain => 'Appfolio.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  )
-=end
+
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
